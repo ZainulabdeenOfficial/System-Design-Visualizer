7616121bc3f2268f75aeddfe172813c7900d8d5d
Zain Ul Abdeen <zu4425@gmail.com>
2025-08-30 14:32:04 +0500

    Gloabal.d.ts fixed

--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,30 @@ declare module 'mdast' {
 declare module 'mdast-util-*' {
   // This prevents TypeScript from trying to find mdast-util types
 }
+
+declare module 'gif.js' {
+  interface GIFOptions {
+    workers?: number;
+    quality?: number;
+    width?: number;
+    height?: number;
+    dither?: boolean | string;
+    transparent?: string | null;
+    background?: string;
+    repeat?: number;
+  }
+
+  interface FrameOptions {
+    delay?: number;
+    copy?: boolean;
+  }
+
+  class GIF {
+    constructor(options?: GIFOptions);
+    addFrame(canvas: HTMLCanvasElement, options?: FrameOptions): void;
+    on(event: string, callback: (data: any) => void): void;
+    render(): void;
+  }
+
+  export default GIF;
+}
